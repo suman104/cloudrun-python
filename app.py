8543b2e0ab8a7ed453b20089eae238539e299fe1
suman <sumansatpathy5@gmail.com>
2021-06-21 14:02:58 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,63 @@
+"""
+A sample Hello World server.
+"""
+import os
+import requests
+
+from flask import Flask, render_template, jsonify, request, abort
+
+# pylint: disable=C0103
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    message = "It's deployed!"
+
+    """Get Cloud Run environment variables."""
+    service = os.environ.get('K_SERVICE', 'Unknown service')
+    revision = os.environ.get('K_REVISION', 'Unknown revision')
+
+    return render_template('index.html',
+        message=message,
+        Service=service,
+        Revision=revision)
+@app.route('/todo/api/v1.0/tasks/<int:taskid>', methods=['GET'])
+def get_tasks(taskid):
+    task= [task for task in tasks if task['id']== taskid]
+    if len(task)== 0:
+        return 404
+    return jsonify({'tasks': task[0]})
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+
+if __name__ == '__main__':
+    server_port = os.environ.get('PORT', '8080')
+    app.run(debug=False, port=server_port, host='0.0.0.0')
